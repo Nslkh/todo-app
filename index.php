6b89a21501857cb9b7aa43e41b2847169aa84fa3
Narzullo <olluzran@gmail.com>
2021-02-01 14:42:04 +0300

    initial setup

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Todo APP</title>
+	<!-- Bootstrap css goes here -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" >
+
+</head>
+<h1 class="lead text-center my-2" >Todo App</h1>
+<body>
+	
+</body>
+</html>
